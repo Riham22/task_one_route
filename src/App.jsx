7fa31fcd64af56a0e3770ebeb29b2aff9e4f5a3c
Riham <rruwainy@gmail.com>
2025-01-18 20:40:16 +0200

    Updated feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,21 @@
 import "./App.css";
-import Star from "./components/Star";
-
-// import NavBar from "./components/NavBar/NavBar";
-// import Hero from "./components/Hero/Hero";
-// import Portfolio from "./components/Portfolio/Portfolio";
-// import About from "./components/About/About";
-// import Contact from "./components/Contact/Contact";
-// import Footer from "./components/Footer/Footer";
+import NavBar from "./components/NavBar/NavBar";
+import Hero from "./components/Hero/Hero";
+import Portfolio from "./components/Portfolio/Portfolio";
+import About from "./components/About/About";
+import Contact from "./components/Contact/Contact";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <>
       <div>
-        {/* <NavBar />
+        <NavBar />
         <Hero />
         <Portfolio />
         <About />
         <Contact />
-        <Footer /> */}
-        <Star />
+        <Footer />
       </div>
     </>
   );
