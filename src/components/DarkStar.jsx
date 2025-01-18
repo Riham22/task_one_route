222830ac59dc76e9dcfdcad07160b63bec743953
Riham <rruwainy@gmail.com>
2025-01-18 19:40:08 +0200

    Initial commit for deployment

---
+++ src/components/DarkStar.jsx
@@ -0,0 +1,35 @@
+import { darkStar } from "../assets/star-dark.svg";
+
+const DarkStar = () => {
+  return (
+    <>
+      <div className="drawing">
+        <hr
+          style={{
+            borderColor: "#333",
+            borderRadius: "2rem",
+            backgroundColor: "#333",
+            opacity: "1",
+            height: "0.2rem",
+            width: "15%",
+          }}
+        />
+        <i className="bi bi-star-fill m-4 ">
+          <img src={darkStar} />
+        </i>
+        <hr
+          style={{
+            borderColor: "#333",
+            borderRadius: "2rem",
+            backgroundColor: "#333",
+            opacity: "1",
+            height: "0.2rem",
+            width: "15%",
+          }}
+        />
+      </div>
+    </>
+  );
+};
+
+export default DarkStar;
